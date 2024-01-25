84453d264b6b3e593bb8300ce73558acdf21ec5f
毛鹏 <729164035@qq.com>
2024-01-25 11:46:34 +0800

    修改邮件的数据模型，增加错误校验；

--- models/tools_model.py
+++ models/tools_model.py
@@ -74,13 +74,6 @@ class EmailModel(BaseModel):
     # 收件人改成自己的邮箱
     send_list: list
 
-    @classmethod
-    def get_obj(cls, data: dict):
-        return cls(send_user=data.get('send_user'),
-                   email_host=data.get('email_host'),
-                   stamp_key=data.get('stamp_key'),
-                   send_list=eval(data.get('send_list')))
-
 
 class WeChatSendModel(BaseModel):
     metrics: TestMetrics
