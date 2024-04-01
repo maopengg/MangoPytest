0f3002f59a90f426b6a619c0b0c972a197c3b2f5
毛鹏 <729164035@qq.com>
2024-04-01 23:25:54 +0800

    修改项目为共享文档的方式来读取数据

--- data/__init__.py
+++ data/__init__.py
@@ -7,9 +7,3 @@ from data.get_project_info import ProjectInfoApi
 
 r = ProjectInfoApi()
 r.main()
-print(r.project)
-print(r.notice_config)
-print(r.test_object)
-print(r.api_info)
-print(r.api_test_case)
-print(r.ui_element)
