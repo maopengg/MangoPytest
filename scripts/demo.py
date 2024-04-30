92ce430367fb861c1e5b48fd6bc5393638e15485
毛鹏 <729164035@qq.com>
2024-04-30 18:35:41 +0800

    优化了执行器报错；

--- scripts/demo.py
+++ scripts/demo.py
@@ -1,11 +1,35 @@
-import lark_oapi as lark
+import pytest
+from playwright.sync_api import Page, expect
+from playwright.sync_api import sync_playwright
 
-# 创建client
-client = lark.Client.builder().app_id("cli_a680e4c8c5be100c").app_secret("uKEDeCAamlviajfBrxXjBhiSKnxONf6x").build()
 
-# 构造请求对象
-request = lark.contact.v3.Spreadsheets.builder().user_id("15c2b353").build()
+@pytest.fixture(scope='function')
+def setup_context_page():
+    with sync_playwright as p:
+        browser = p.chromium.launch(headless=False,
+                                    executable_path=r'C:\Program Files\Google\Chrome\Application\chrome.exe')
+        page = browser.new_page()
+        return page
 
-# 发起请求
-response = client.contact.v3.user.get(request)
-print(response.msg)
+
+def test_example(page: Page) -> None:
+    page.goto("https://www.baidu.com/")
+    page.get_by_role("link", name="登录").click()
+    page.get_by_placeholder("手机号/用户名/邮箱").click()
+    page.get_by_placeholder("手机号/用户名/邮箱").fill("18311527751")
+    page.get_by_text(
+        "扫码登录账号登录短信登录验证即登录，未注册将自动创建百度账号发送验证码收不到短信验证码?物联网卡手机号未修改，被限制登录去解禁您账号存在安全风险被限制登录去解禁").click()
+    page.get_by_text("扫码登录账号登录短信登录").click()
+    page.get_by_placeholder("请输入手机号").click()
+    page.get_by_placeholder("请输入手机号").fill("18311527751")
+    page.get_by_role("textbox", name="验证码").click()
+    page.get_by_role("textbox", name="验证码").click()
+    page.get_by_role("checkbox", name="阅读并接受").check()
+    page.get_by_role("textbox", name="验证码").click()
+    page.get_by_role("textbox", name="验证码").click()
+    page.get_by_role("button", name="发送验证码").click()
+    page.get_by_role("textbox", name="验证码").click()
+    page.get_by_role("textbox", name="验证码").click()
+    page.get_by_role("textbox", name="验证码").fill("969439")
+    page.get_by_role("button", name="登录").click()
+    expect(page.locator("#su")).to_contain_text("百度一下")
