86d451943acb008cfe3b962ad6ddacf81d88e491
毛鹏 <729164035@qq.com>
2024-11-19 11:23:26 +0800

    优化一个内容

--- tools/base_request/request_tool.py
+++ tools/base_request/request_tool.py
@@ -18,31 +18,32 @@ class RequestTool:
     data_processor: DataProcessor = None
 
     @log_decorator
-    def http(self, data: ApiDataModel) -> ApiDataModel | Response:
+    def http(self, data: ApiDataModel, is_replace=True) -> ApiDataModel | Response:
         """
         处理请求的数据，写入到request对象中
-        @return:
+        :param data: ApiDataModel
+        :param is_replace: 是否过滤请求中的${}, 如果数据本身就有${}，那需要传false
+        :return:
         """
         data.request.url = urljoin(data.base_data.host, data.request.url)
-        is_replace = self.data_processor.get_cache('IS_REPLACE')
-        if is_replace is None:
-            is_replace = True
-        for key, value in data.request:
-            if value is not None and key != 'file':
-                if is_replace:
-                    value = self.data_processor.replace(value)
-                    setattr(data.request, key, value)
-            elif key == 'file':
-                if data.request.file:
-                    file = []
-                    for i in data.request.file:
-                        i: dict = i
-                        for k, v in i.items():
-                            file_name = self.data_processor.identify_parentheses(v)[0].replace('(', '').replace(')', '')
-                            if is_replace:
-                                path = self.data_processor.replace(v)
-                                file.append((k, (file_name, open(path, 'rb'))))
-                    data.request.file = file
+        if is_replace:
+            for key, value in data.request:
+                if value is not None and key != 'file':
+                    if is_replace:
+                        value = self.data_processor.replace(value)
+                        setattr(data.request, key, value)
+                elif key == 'file':
+                    if data.request.file:
+                        file = []
+                        for i in data.request.file:
+                            i: dict = i
+                            for k, v in i.items():
+                                file_name = self.data_processor \
+                                    .identify_parentheses(v)[0].replace('(', '').replace(')', '')
+                                if is_replace:
+                                    path = self.data_processor.replace(v)
+                                    file.append((k, (file_name, open(path, 'rb'))))
+                        data.request.file = file
         data.response = self.http_request(data.request)
         return data
 
