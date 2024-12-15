b2b3c612729d2b5bb8b5e980fb4a719164274817
毛鹏 <729164035@qq.com>
2024-12-15 23:22:15 +0800

    优化了一些问题；修复了一些bug；

--- exceptions/__init__.py
+++ exceptions/__init__.py
@@ -9,14 +9,13 @@ from tools.log import log
 
 class PytestAutoTestError(Exception):
 
-    def __init__(self, code: int, msg: str, value: tuple = None, error: any = None, is_log: bool = True):
+    def __init__(self, code: int, msg: str, value: tuple = None, error: any = None):
         self.code = code
         if value:
             self.msg = msg.format(*value)
-        if error and is_log:
-            log.error(f'报错提示：{self.msg}， 报错内容：{error}')
         else:
-            log.error(f'报错提示：{self.msg}')
+            self.msg = msg
+        log.error(f'报错提示：{self.msg}， 报错内容：{error}')
 
 
 class UiError(PytestAutoTestError):
