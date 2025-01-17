7fb0c96b1e47c84101e7f6c8aa557787ea16326f
毛鹏 <729164035@qq.com>
2025-01-17 10:42:58 +0800

    修复一个问题；

--- scripts/B.py
+++ scripts/B.py
@@ -1,16 +1,5 @@
-from multiprocessing import Process, Manager
+# This is the sshd server system-wide configuration file.  See
+# sshd_config(5) for more information. 
 
+# This sshd was compiled with PATH=/usr/local/bin:/usr/bin:/bin:/usr/games 
 
-def f(shared_dict):
-    shared_dict['key'] = 'value'  # 修改共享字典
-
-
-if __name__ == '__main__':
-    manager = Manager()
-    shared_dict = manager.dict()  # 创建一个共享字典
-
-    p = Process(target=f, args=(shared_dict,))
-    p.start()
-    p.join()
-
-    print(f'共享的字典: {shared_dict}')
