18563e7d6b54c3b3a28a263053040391aa9ed5b3
毛鹏 <729164035@qq.com>
2025-03-04 21:19:38 +0800

    修复了一个问题

--- auto_test/api_mango_testing_platform/modules_api/ui_auto/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Project: 芒果测试平台
-# @Description: 
-# @Time   : 2024-04-23 11:19
-# @Author : 毛鹏
--- auto_test/project_config.py
+++ auto_test/project_config.py
@@ -9,16 +9,13 @@ from enums.tools_enum import AutoTestTypeEnum
 
 class ProjectEnum(BaseEnum):
     WanAndroid = '玩安卓'
-    BaiduTranslate = '百度翻译'
     Gitee = 'gitee'
-    Mango = '芒果测试平台'
+    SQL = 'sql'
 
 
 auto_test_project_config = [
     {'type': AutoTestTypeEnum.API, 'project_name': ProjectEnum.WanAndroid, 'dir_name': 'api_wan_android'},
     {'type': AutoTestTypeEnum.UI, 'project_name': ProjectEnum.WanAndroid, 'dir_name': 'ui_wan_android'},
-    {'type': AutoTestTypeEnum.API, 'project_name': ProjectEnum.BaiduTranslate, 'dir_name': 'api_baidu_translate'},
-    {'type': AutoTestTypeEnum.API, 'project_name': ProjectEnum.Mango, 'dir_name': 'api_mango_testing_platform'},
-    {'type': AutoTestTypeEnum.UI, 'project_name': ProjectEnum.Gitee, 'dir_name': 'ui_gitee'},
-    {'type': AutoTestTypeEnum.OTHER, 'project_name': ProjectEnum.Mango, 'dir_name': 'sql_auto'},
+    {'type': AutoTestTypeEnum.UI, 'project_name': ProjectEnum.Gitee, 'dir_name': 'ui_baidu'},
+    {'type': AutoTestTypeEnum.OTHER, 'project_name': ProjectEnum.SQL, 'dir_name': 'sql_auto'},
 ]
