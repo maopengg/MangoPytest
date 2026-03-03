739df5078e0389588a9dfa07600ef530369031ff
毛鹏 <729164035@qq.com>
2026-03-03 15:22:14 +0800

    优化分页问题

--- auto_test/ui_baidu/test_case/test_search/test_open_source.py
+++ auto_test/ui_baidu/test_case/test_search/test_open_source.py
@@ -20,24 +20,8 @@ class TestOpenSource:
     base_data_model = base_data_model
     test_data: ObtainTestData = ObtainTestData()
 
-    @case_data([1, ])
+    @case_data([1, 2])
     def test_01(self, base_data, data: UiDataModel):
         login_page = HomePage(base_data, self.base_data_model, self.test_data)
         login_page.goto()
         login_page.search_keyword(data.test_case.data.get('name'))
-        search_results_page = SearchResultsPage(base_data, self.base_data_model, self.test_data)
-        text = search_results_page.enter_the_project(data.test_case.data.get('name'))
-        assert text is not None, f'断言失败，返回的值是：{text}'
-        search_results_page.w_wait_for_timeout(3)
-
-    @case_data(3)
-    def test_02(self, base_data, data: UiDataModel):
-        login_page = HomePage(base_data, self.base_data_model, self.test_data)
-        login_page.goto()
-        login_page.search_keyword()
-
-    @case_data(case_name='进入到mangopytest主页')
-    def test_03(self, base_data, data: UiDataModel):
-        login_page = HomePage(base_data, self.base_data_model, self.test_data)
-        login_page.goto()
-        login_page.search_keyword()
